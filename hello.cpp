882501cab2aa6c6ac823be52b630457fcd269a62
lickkc <likhithakchiranjeevi@gmail.com>
2025-03-13 16:18:16 +0530

    Updated hello.cpp to trigger Jenkins build

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this PES1UG22CS307 \n";
+  cout << "I have successfully built and run this PES1UG22CS307-2 \n";
+  cout << "version 2\n";
   return 0;
 }
